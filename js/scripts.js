35d469216128aa50bf3d985e1a3ee520d2e63bff
Marcel Nogueira and Zach Bastian <zach@>
2017-03-21 16:36:01 -0700

    it will add pictures when clicking buttons, and delete when clicking pictures.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
     $("ul#user").prepend("<li>Meow!</li>");
-    $("ul#user").prepend("<img src=img/cat.jpg>");
+    $("ul#user").prepend("<img src='img/cat.jpg'>");
     $("ul#webpage").prepend("<li>Bark!</li>");
-    $("ul#webpage").prepend("<img src=img/dog.jpg>");
-    $("ul#user").children('li').first().click(function() {
+    $("ul#webpage").prepend("<img src='img/dog.jpg'>");
+    $("ul#user").children('img').first().click(function() {
       $(this).remove()
-      alert('Meow/woof!');
+    $("ul#webpage").children('img').first().click(function() {
+      $(this).remove()
+      });
     });
   });
 
   $("button#dog").click(function() {
     $("ul#user").prepend("<li>Bark!</li>");
-    $("ul#user").prepend("<img src=img/dog.jpg>");
+    $("ul#user").prepend("<img src='img/dog.jpg'>");
     $("ul#webpage").prepend("<li>Meow!</li>");
-    $("ul#webpage").prepend("<img src=img/cat.jpg>");
+    $("ul#webpage").prepend("<img src='img/cat.jpg'>");
+    $("ul#user").children('img').first().click(function() {
+      $(this).remove()
+    $("ul#webpage").children('img').first().click(function() {
+      $(this).remove()
+      });
+    });
   });
 
   $("button#turtle").click(function() {
     $("ul#user").prepend("<li>Zzzzz...</li>");
-    $("ul#user").prepend("<img src=img/turtle.jpg>");
+    $("ul#user").prepend("<img src='img/turtle.jpg'>");
     $("ul#webpage").prepend("<li>The dog and cat look confused.</li>");
-    $("ul#webpage").prepend("<img src=img/dogcat.jpg>");
+    $("ul#webpage").prepend("<img src='img/dogcat.jpg' width=50%>");
   });
 });
